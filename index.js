fcff7d8673a5524f18e8ceb000d967700ea9a603
nairx <nairxprv@gmail.com>
2025-07-28 13:34:10 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 const app = express();
 app.use(express.json());
+app.use(cors());
 mongoose
   .connect("mongodb://localhost:27017/grietdb")
   .then(() => {
