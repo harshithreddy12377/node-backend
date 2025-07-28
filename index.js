33028ea0e9bfd0be1324e1b99e76fba797acbde3
nairx <nairxprv@gmail.com>
2025-07-28 10:20:46 +0530

    updated

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("Hello World")
-console.log("Good Morning")
+import express from 'express'
+const app = express()
+app.listen(8080, () => {
+  console.log('Server is running on port 8080')
+})  
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})  
