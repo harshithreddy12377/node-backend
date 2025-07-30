67f397ab061b32efa2386a0af8b266bc5bcb85ef
harshithreddy12377 <harshithreddy12377@gmail.com>
2025-07-30 15:05:54 +0530

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 import {showProducts} from "../controllers/productController.js";
 import express from "express";
 const router = express.Router();
 router.get("/", showProducts);
-=======
+
 import { showProducts,deleteProduct,updateProduct,createproduct } from "../controllers/productController.js";
 import express from "express";
 const router = express.Router();
@@ -11,5 +11,5 @@ router.get("/showproducts", showProducts);
 router.delete("/deleteproduct/:id",deleteProduct)
 router.patch("/updateproduct/:id",updateProduct)
 router.post("/createproduct",createproduct)
->>>>>>> 4928f42dfc3d330cf90c7e63b20c2532709481f3
+
 export default router;
